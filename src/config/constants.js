99fddc90154a18ca8745d78324ac64c787edf644
sreekarteegala <teegalasreekar@gmail.comn>
2024-06-14 00:36:36 +0530

    Exposed constants API

--- src/config/constants.js
+++ src/config/constants.js
@@ -10,12 +10,6 @@ const USER_ACTIVATION_STATUS = {
   PENDING: "pending",
 };
 
-const DOCUMENT_TYPES = {
-  COL_APP: "College Applicatin Essay",
-  LOR: "Letter of Recommendation",
-  RESUME: "Resume",
-  PRD: "Product Requirement Document",
-};
 const REVIEW_STATUS = {
   INPROGRESS: "inProgress",
   COMPLETED: "completed",
@@ -44,7 +38,6 @@ const URL_REGEX = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
 module.exports = {
   ROLES,
   USER_ACTIVATION_STATUS,
-  DOCUMENT_TYPES,
   REVIEW_STATUS,
   DOMAIN_VALUES,
   INDUSTRY_VALUES,
